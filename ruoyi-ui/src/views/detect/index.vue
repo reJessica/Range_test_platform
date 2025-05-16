9e2ac0f9682c417eab93b052df6b273b187c5d8b
bubblebxr <1836821978@qq.com>
2025-05-16 18:29:35 +0800

    1

---
+++ ruoyi-ui/src/views/detect/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: url(../../assets/ai/backround1.jpg) no-repeat;
+    background-size: 100% 100%;
+}
+</style>
